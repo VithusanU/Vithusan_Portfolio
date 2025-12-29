1fdb78031bfc65ad16a5727bf924c1be44541119
VithusanU <130177580+VithusanU@users.noreply.github.com>
2025-12-29 13:25:52 -0500

    fixing changes

--- src/pages/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Vithusan Uruthirakumaran</h1>
-      <p>Product Manager & Software Developer</p>
-      <p>Building innovative solutions with a focus on user experience and technical excellence.</p>
-    </main>
-  );
-}
